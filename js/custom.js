d88549a3ee6d2c07053a9c57d49ae4d1930a2ac4
unknown <lyly125@live.cn>
2023-07-09 13:35:58 +0800

    Menu update, mobile style fixed

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,10 @@ $(document).ready(function() {
 	$(".btn.btn-outline-secondary.menu-ico").click(function(){
 		$(".main-menu-item").toggleClass("d-none");
 	});
+	
 	$(".main-menu-item .has-submenu").click(function(){
-		$(".main-menu-item .sub-menu.elementor-nav-menu--dropdown").toggleClass("d-none");
-		$(".main-menu-item .has-submenu .glyphicon").toggleClass("glyphicon-menu-down");
-		$(".main-menu-item .has-submenu .glyphicon").toggleClass("glyphicon-menu-up");
-	});
+		$(this).siblings(".sub-menu.elementor-nav-menu--dropdown").toggleClass("d-none");
+		$(this).children(".has-submenu .glyphicon").toggleClass("glyphicon-menu-down");
+		$(this).children(".has-submenu .glyphicon").toggleClass("glyphicon-menu-up");
+	})
 });
